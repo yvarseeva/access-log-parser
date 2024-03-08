6a542b3adf178ed8eb728ae2a8563176571adbbc
Yulia Varseeva <ulyasha1406@rambler.ru>
2024-03-08 22:45:27 +0300

    META-file added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Случайное число от 0 до 1:" +
-                Math.random());
+    public static void main(String[] args) {
+        System.out.println("Введите текст и нажмите <Enter>: ");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста:" + text.length());
     }
 }
